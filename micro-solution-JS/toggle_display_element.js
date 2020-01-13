8255c4e70a8daf99f588b2c63453fa59bdc2246d
gustavovaler <gustavodevaler@gmail.com>
2020-01-13 01:57:49 -0300

    Toggle function

--- micro-solution-JS/toggle_display_element.js
+++ micro-solution-JS/toggle_display_element.js
@@ -1,6 +1,7 @@
 
 
-
+//This function gets 2 parameters. The first is the element you want to show or hide.
+// the second is opcional and  gives us the possibilty of delaying this action in miliseconds.
 
 function toggle(e,time = 0){
 	let element = window.getComputedStyle(e);
@@ -14,11 +15,49 @@ function toggle(e,time = 0){
 	}else if (elementVisibility === 'block' || elementStyleVisibility === 'visible' || elementStyleDisplay === 'block') {
 		hide(e);
 		}
-		function hide(element_to_hide){
+		function hide(element){
 
-				setTimeout(function(){elemento.style.display = "none";},time);
+				setTimeout(function(){element.style.display = "none";},time);
 			}
-		function show(element_to_show){
-				setTimeout(function(){elemento.style.display = "block";},time);
+		function show(element){
+				setTimeout(function(){element.style.display = "block";},time);
 			}	
 	}
+
+	//use example:
+
+//	<script src="toggle_display_element.js"></script>
+
+//		<button onclick="toggle(element)">Show or hide</button>
+
+//		<div id="some_div">
+
+//			<p> Content what you want toggle</p>
+
+//   		<span> More content</span>
+//		</div>
+
+//	<script >
+
+//	var element = document.getElementById('some_div');
+
+//	</script>
+
+// -------------- 
+// Another way:
+
+//	<script src="toggle_display_element.js"></script>
+//	<span id="trigger">Click me for Show or hide</span>
+
+//	<div id="some_div">
+//		<p> Content what you want toggle</p>
+//    	<span> More content</span>
+//	</div>
+
+//	<script >
+//		var element = document.getElementById('some_div');
+//		var trigger = document.getElementById('trigger');
+//		trigger.addEventListener("click", function(){
+//		toggle(element);
+//		});
+//	</script>
